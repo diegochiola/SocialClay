6052f6b0d0d0eff3267b2336a21bb287ece2c167
diega <diegochiola@gmail.com>
2024-03-11 12:51:06 +0100

    creation of Ceramic Challenges controller and model

---
+++ app/Http/Controllers/CeramicChallengeController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CeramicChallenge;
+use App\Models\CeramiChallenge;
+
+use Illuminate\Http\Request;
+
+class ceramicChallengeController extends Controller
+{
+    /*
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $ceramicChallenges = CeramicChallenge::all();
+        return view('ceramicChallenges.index', ['artworks' => $ceramicChallenges]);
+
+        //$ceramicChallenges = ceramicCh$ceramicChallenge::all();
+
+        //return view('ceramicChallenges.index');
+    }
+
+    /*
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('ceramicChallenges.create');
+    }
+
+    /*
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // validations
+        $request->validate([
+            'title' => 'required|unique:ceramic_challenges|max:50',
+            'description' => 'required|max:255',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            
+        ]);
+    
+        // save data
+        $ceramicChallenge = new CeramicChallenge();
+        $ceramicChallenge->title = $request->input('title');
+        $ceramicChallenge->description = $request->input('description');
+        $ceramicChallenge->start_date = $request->input('start_date');
+        $ceramicChallenge->end_date = $request->input('end_date');
+        $ceramicChallenge->save(); 
+        return view("ceramicChallenges.message", ['msg' => "Ceramic Challenge created successfully!"]);
+    }
+
+    /*
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+
+    }
+
+    /*
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+               //recibe el iddel artwork
+               $ceramicChallenge = ceramicChallenge::find($id); //busque por el id
+               return view('ceramicChallenges.edit', ['ceramicChallenge' => $ceramicChallenge]);
+    }
+
+    /*
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        //first found the id
+        $ceramicChallenge = CeramicChallenge::findOrFail($id);
+                // validations
+                $request->validate([
+                    'title' => 'required|unique:ceramic_chellenges,title,'.$id.',id|max:50', //debera ignorar el unique porque esta editando el mismo elemento
+                    'description' => 'required|max:255',
+                    'start_date' => 'required',
+                    'end_date' => 'required',
+       
+                ]);
+            
+                // save data
+                $ceramicChallenge->title = $request->input('title');
+                $ceramicChallenge->description = $request->input('description');
+                $ceramicChallenge->start_date = $request->input('start_date');
+                $ceramicChallenge->end_date = $request->input('end_date');
+                $ceramicChallenge->save(); // lo guardamos
+            
+                return view("ceramicChallenges.message", ['msg' => "Ceramic challenge updated successfully!"]);
+            }
+    
+
+    /*
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+            
+            $ceramicChallenge = CeramicChallenge::find($id); //buscamos por el id
+            $ceramicChallenge->delete();
+            return view("ceramicChallenges.message", ['msg' => "Ceramic Challenge deleted successfully!"]);
+    }
+}
